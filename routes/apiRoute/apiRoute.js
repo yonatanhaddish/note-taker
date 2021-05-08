6c328591619714e96d4550ece9948401b49c19c7
Your Name <you@example.com>
2021-05-08 14:45:57 -0400

    working on delete button

--- routes/apiRoute/apiRoute.js
+++ routes/apiRoute/apiRoute.js
@@ -1,20 +1,27 @@
 const router= require('express').Router();
 const db = require('../../data/db.json');
-const { createNewTask, validateTask } = require('../../lib/db');
+const { createNewTask, validateTask, findById } = require('../../lib/db');
 
 router.get('/notes', (req, res) => {
     let results= db;
     console.log(results);
-    res.json(results);
+    res.json(results);  
+});
 
+router.get('/notes/:id', (req, res) => {
+    const result= findById(req.params.id, db);
     
+    if(result) {
+        res.json(result);
+    }
+    else {
+        res.send(404);
+    }
 });
 
 router.post('/notes', (req, res) => {
     
     res.json(req.body);
-    // console.log('testtest' + req.body);
-    // here we have to create a unique ID for each data task
 
     if (!validateTask(req.body)) {
         res.status(400).send('The task tilte and text should not be empty!');
@@ -24,11 +31,5 @@ router.post('/notes', (req, res) => {
         res.json(task);
     }
 });
-
-
-
-
-
-
 
 module.exports= router;
